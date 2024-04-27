0f82ee7b83d5746d769e393de8599326c22be76c
Chiunmi <jeninecarpio622@gmail.com>
2024-04-27 10:02:38 +0800

    updated 042724 viewport di ko knows kung nagana

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Sidebar from "./Components/Navbar/Sidebar/Sidebar";
 import Right from "./Components/Navbar/Right/Right";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <Right />
-    </div>
+    <Router>
+      <div>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <Navbar />
+        <Sidebar />
+        <Right />
+      </div>
+    </Router>
   );
 };
 
